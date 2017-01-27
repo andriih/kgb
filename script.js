9e7c4d3d57d4c86a84e8acb0b2472087cbf6bb41
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2017-01-27 13:33:24 +0200

    done

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 var form = document.forms.addNeighborsForm,
 	fullName = form.elements.fullName,
 	msg = document.getElementsByClassName('msg'),
-	//adresErr = document.getElementById('msg2'),
 	adres = form.elements.adres,
 	addBtn = form.elements.btn,
 	btn = '<button type="submit" name="btn">Remove</button>',
 	check = '<input type="checkbox" name="inportant">',
 	showNeighbors = document.getElementById('showNeighbors');
 
+fullName.value.trim();
+adres.value.trim();
 
 form.addEventListener("submit",function(e){
-	var nameVal = fullName.value.trim();
-	var 
-
+	
 	if (fullName.value.length===0) {
 		msg[0].innerText = "Name cant be empty!";
 		e.preventDefault();
@@ -27,36 +26,34 @@ form.addEventListener("submit",function(e){
 	}
 
 	if(fullName.value.length != 0 && adres.value.length != 0){
-		fullName.innerHtml ="";	
-		adres.innerText ="";	
+	
+		var newDiv = document.createElement("div");
+
+		var fullNameField = document.createElement("span");
+		var adresField = document.createElement("span");
+		var btnField = document.createElement("span");
+		var checkField = document.createElement("span");
+
+
+		fullNameField.innerText = fullName.value.trim();
+		adresField.innerText = adres.value.trim();
+		btnField.innerHTML = btn;
+		checkField.innerHTML = check;
+		
+
+		newDiv.appendChild(fullNameField);
+		newDiv.appendChild(adresField);
+		newDiv.appendChild(checkField);
+		newDiv.appendChild(btnField);
+		
+		showNeighbors.appendChild(newDiv);
+
+		fullName.value ="";	
+		adres.value ="";
+
 	}else{
 		e.preventDefault();
 	}
-
-});
-addBtn.addEventListener("click",function(){
-	var newDiv = document.createElement("div");
-
-	var fullNameField = document.createElement("span");
-	var adresField = document.createElement("span");
-	var btnField = document.createElement("span");
-	var checkField = document.createElement("span");
-
-
-	fullNameField.innerText = fullName.value.trim();
-	adresField.innerText = adres.value.trim();
-	btnField.innerHTML = btn;
-	checkField.innerHTML = check;
-	
-
-	newDiv.appendChild(fullNameField);
-	newDiv.appendChild(adresField);
-	newDiv.appendChild(checkField);
-	newDiv.appendChild(btnField);
-	
-
-	showNeighbors.appendChild(newDiv);
-	console.log(newDiv);
 
 });
 
